809172084ed6cc5185d20eee17417e5498732e84
nnguyen26 <nndphuong@gmail.com>
2026-01-25 21:18:41 -0600

    feat: add code get ip from server

--- news_trade/server.py
+++ news_trade/server.py
@@ -15,13 +15,13 @@ import apprise
 def main():
     config = Config()
     logger = Logger(config, "news_trade_server")
-    logger.info(Message(title = f"Start News Trade - Time: {datetime.fromtimestamp(int(time.time()), tz=pytz.timezone('America/Chicago'))}", body=f"{json.dumps(config.beautify(), indent=2)}", format=apprise.NotifyFormat.TEXT), True)
+    logger.info(Message(title = f"Start News Trade - Time: {datetime.fromtimestamp(int(time.time()), tz=pytz.timezone('Asia/Ho_Chi_Minh'))}", body=f"{json.dumps(config.beautify(), indent=2)}", format=apprise.NotifyFormat.TEXT), True)
 
     twitter = Twitter(config, logger)
     threads = Threads(config, logger)
     telegram = Telegram(config, logger)
     schedule = SafeScheduler(logger)
-    command = Command(config)
+    command = Command(config, logger)
     command.start_bot()
     schedule.every(config.THREADS_SCRAPE_SLEEP_TIME).seconds.do(threads.scrape_user_posts).tag("threads_scrape_news")
     schedule.every(config.TWITTER_SCRAPE_SLEEP_TIME).seconds.do(twitter.scrape_user_tweets).tag("twitter_scrape_news")
